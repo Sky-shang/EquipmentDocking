551da96eebd928c482ab1a2081ae71cf1f22d25c
Administrator <yuankun.shang@globetools.com>
2020-04-30 14:50:14 +0800

    delete model viewmodel

---
+++ MVVMLight/Models/WelcomeModel.cs
@@ -0,0 +1,23 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMLight.Models
+{
+    public class WelcomeModel : ObservableObject
+    {
+        private String introduction;
+        /// <summary>
+        /// 欢迎词
+        /// </summary>
+        public String Introduction
+        {
+            get { return introduction; }
+            set { introduction = value; RaisePropertyChanged(() => Introduction); }
+
+        }
+    }
+}
